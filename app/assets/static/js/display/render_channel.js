2819144c068d83dc3555617b41900f10dac85946
星灿长风v <2452234@st.shou.edu.cn>
2026-02-05 22:54:00 +0800

    给后端帖子类加了新的触发器; 加入了基础的频道支持, 前端也加入了相关方法

---
+++ app/assets/static/js/display/render_channel.js
@@ -0,0 +1,44 @@
+async function loadChannel() {
+    const response = await fetch(
+        "/api/channel",
+        { method: "GET" }
+    );
+    if (response.status===200) {
+        return await response.json()
+    } return [];
+}
+
+async function renderChannel() {
+    console.log("render channel");
+    const channelData = await loadChannel();
+    const channelList = DOMBuilder.ul({
+        className: "postsList"
+    });
+    if (channelData && channelData.length > 0) {
+        channelData.forEach(channel => {
+            const channelItem = DOMBuilder.li({
+                className: "post channel",
+                id: channel.channel_id,
+                children: [
+                    DOMBuilder.h2({
+                        text: channel.channel_name
+                    }),
+                    (() => {
+                        return DOMBuilder.div({
+                            className: "channelDesc",
+                            text: channel.description
+                        });
+                    })()]
+            });
+            channelList.append(channelItem);
+        });
+    } else {
+        const noChannel = DOMBuilder.li({
+            text: "暂无频道",
+            className: "post"
+        });
+        channelList.append(noChannel)
+    }
+    channelContainer.append(channelList);
+    whenClick();
+}
